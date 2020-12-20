0dd80caf08be9bca2882ee9c5e685bfe62c8c6ff
ian simpson <iansimpson55@hotmail.com>
2020-12-20 14:35:09 +0000

    Added "answers" array

--- WebApplication1/Controllers/TestController.cs
+++ WebApplication1/Controllers/TestController.cs
@@ -6,5 +6,10 @@ namespace WebApplication1.Controllers
     [Route("[controller]")]
     public class TestController : ControllerBase
     {
+        private static readonly string[] Answers = new[]
+        {
+            "Yes", "No"
+        };
+
     }
 }
